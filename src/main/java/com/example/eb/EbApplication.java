204a0e4158ee5ddf390083b391547608686d52fb
suddii01 <suddii01@naver.com>
2024-01-05 01:23:19 +0900

    Initial Project Setup

---
+++ src/main/java/com/example/eb/EbApplication.java
@@ -0,0 +1,13 @@
+package com.example.eb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eb/EbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
